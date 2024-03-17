71e5df6bb5bbae77332f73f1add88183c8cfdba9
ISAWASHUN <syun20000616@icloud.com>
2024-03-17 14:59:02 +0900

    iterator

---
+++ 04-design-pattern-part1/adapter/adapterInheritance.ts
@@ -0,0 +1,41 @@
+export {}
+
+interface Target {
+    getCsvData(): string;
+}
+
+class NewLibrary {
+    getJsonData() {
+        return [
+            {
+                "data1": "json_dataA",
+                "data2": "json_dataB",
+            },
+            {
+                "data1": "json_dataC",
+                "data2": "json_dataD",
+            }
+        ];
+    }
+}
+
+class JsonToCsvAdapter extends NewLibrary implements Target {
+    getCsvData(): string {
+        const jsonData = this.getJsonData();
+
+        const header = Object.keys(jsonData[0]).join(",") + "\n";
+        const body = jsonData.map(d => {
+            return Object.keys(d).map(key => d[key]).join(",");
+        }).join("\n");
+
+        return header + body;
+    }
+}
+
+function run() {
+    const adapter = new JsonToCsvAdapter();
+    console.log('=== Adapterに変換されたデータ ===');
+    console.log(adapter.getCsvData());
+}
+
+run();
